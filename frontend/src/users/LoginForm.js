7624e6a21046f45039ef9e7547ee24590dbbb488
Matthew Wolfe <thewolfeslayer@gmail.com>
2023-08-06 09:46:53 -0400

    jwt authentication done

--- frontend/src/users/LoginForm.js
+++ frontend/src/users/LoginForm.js
@@ -14,7 +14,7 @@ function LoginForm() {
     })
 
     const [errorMessage, setErrorMessage] = useState(null)
-      
+
     async function handleSubmit(e) {
         e.preventDefault()
         const response = await fetch(`http://localhost:5000/authentication/`, {
@@ -29,12 +29,13 @@ function LoginForm() {
 
         if (response.status === 200) {
             setCurrentUser(data.user)
+            localStorage.setItem('token', data.token)
             history.push(`/`)
         } else {
             setErrorMessage(data.message)
         }
     }
-  
+
     return (
         <main>
             <h1>Login</h1>
